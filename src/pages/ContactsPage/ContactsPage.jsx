e61e2511c44207f82bf0ffc13f5f11345cf1e23f
Pablo <st.spasam@gmail.com>
2024-05-19 23:31:10 +0300

    fix

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import { fetchContacts } from "../../redux/contacts/operations";
